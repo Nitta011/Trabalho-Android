8551160ad1197fe7e7832792ebd4e46a569c1c1f
Gabriel Nitta <gnitta75@gmail.com>
2022-11-07 21:35:05 -0300

    Atualizando o projeto 1.0

--- ProjetoCarlos/app/src/main/java/com/example/projetocarlos/ui/activity/FormularioPessoasActivity.kt
+++ ProjetoCarlos/app/src/main/java/com/example/projetocarlos/ui/activity/FormularioPessoasActivity.kt
@@ -1,13 +1,12 @@
-package com.example.projetocarlos
+package com.example.projetocarlos.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projetocarlos.R
 
 class FormularioPessoasActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_pessoas)
     }
-
 }
