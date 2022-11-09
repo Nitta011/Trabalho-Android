f42517fc340e1222cfbd86ae815fe61c001a69e0
Gabriel Nitta <gnitta75@gmail.com>
2022-11-09 19:39:56 -0300

    Finalização do projeto

--- ProjetoCarlos/app/src/main/java/com/example/projetocarlos/ui/activity/ListaPessoasActivity.kt
+++ ProjetoCarlos/app/src/main/java/com/example/projetocarlos/ui/activity/ListaPessoasActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.projetocarlos.R
 import com.example.projetocarlos.dao.PessoasDao
+import com.example.projetocarlos.database.dao.AppDatabase
 import com.example.projetocarlos.ui.recyclerview.adapter.ListaPessoasAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -17,6 +19,24 @@ class ListaPessoasActivity : AppCompatActivity(R.layout.activity_lista_pessoas) 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         configuraRecyclerView()
+
+        val db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java, "projetocarlos.db"
+        ).allowMainThreadQueries()
+            .build()
+        val pessoaDao = db.pessoasDao()
+        //   pessoaDao.salva(
+        //     Pessoas(
+        //       nome = "Teste" ,
+        //     dt_nascimento = "12",
+        //   telefone = "22",
+        // email = "A",
+        //rua = "C",
+        //cep = "1",
+        //     )
+        //  )
+        adapter.att(pessoaDao.buscaTodos())
     }
 
     override fun onResume() {
@@ -29,7 +49,7 @@ class ListaPessoasActivity : AppCompatActivity(R.layout.activity_lista_pessoas) 
     private fun configuraFab() {
         val fab = findViewById<FloatingActionButton>(R.id.pessoa_floatingActionButton)
         fab.setOnClickListener {
-            val intent =  Intent(this, FormularioPessoasActivity::class.java)
+            val intent = Intent(this, FormularioPessoasActivity::class.java)
             startActivity(intent)
         }
     }
